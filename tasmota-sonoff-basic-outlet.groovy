eecf3f99880a23e98adc0c428a2daef6a1c62207
Junliu589 <53349608+Junliu589@users.noreply.github.com>
2019-10-05 10:27:17 -0600

    Update tasmota-sonoff-basic-outlet.groovy

--- tasmota-sonoff-basic-outlet.groovy
+++ tasmota-sonoff-basic-outlet.groovy
@@ -1,20 +1,4 @@
-/**
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- *  Author: Jun Liu
- *
- *  Date:09/02/2019
- */
- 
- 
+
 metadata {
     definition (name: "Tasmota Sonoff Basic Wifi Outlet", namespace: "LJ", author: "LJ", vid:"tasmota-sonoff-basic-switch") {
         capability "Actuator"
@@ -53,8 +37,8 @@ metadata {
     preferences {
         input name: "ipAddr", type: "text", title: "IP Address", description: "IP Address of the device", required: true,displayDuringSetup: true
         input name: "port", type: "number", title: "Port", description: "Port of the device",  defaultValue: 80 ,displayDuringSetup: true
-        input name: "username", type: "text", title: "Username", description: "Username to manage the device", required: true, displayDuringSetup: true
-        input name: "password", type: "password", title: "Password", description: "Username to manage the device", required: true, displayDuringSetup: true
+        input name: "username", type: "text", title: "Username", description: "Username to manage the device", required: false, displayDuringSetup: true
+        input name: "password", type: "password", title: "Password", description: "Username to manage the device", required: false, displayDuringSetup: true
     }
 
 }
@@ -147,7 +131,7 @@ def checkDevice() {
         sendEvent(name: "deviceStatus", value: "offline")
     }
     
-    state.responseReceived = false;
+    state.responseReceived = false
     tasmotaHttpCmd("Power")
 }
 
